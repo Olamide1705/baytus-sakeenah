af796761f57beb1c8eae5f5bc05ab04ee20c7e33
Abdrahman Oladimeji <abdrahmanoladimeji02@gmail.com>
2025-06-03 22:36:06 +0100

    fix: move form submission logic to the component for proper error handling

---
+++ src/utils/env.ts
@@ -0,0 +1,2 @@
+
+export const baseUrl = 'https://0181-102-89-83-74.ngrok-free.app'
--- src/utils/formUtils.ts
+++ src/utils/formUtils.ts
@@ -1,6 +1,4 @@
-import axios from 'axios';
 import { FormData } from '../types';
-
 
 export const validateEmail = (email: string): boolean => {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -35,32 +33,3 @@ export const validateForm = (formData: FormData): Record<string, string> => {
   return errors;
 };
 
-const baseUrl = 'https://0181-102-89-83-74.ngrok-free.app'
-
-export const handleFormSubmit = async (formData: FormData): Promise<boolean> => {
-  console.log('Form submitted:', formData);
-
-  try {
-    const response = await axios.post(`${baseUrl}/api/v1/waitlist/`, formData);
-
-    console.log('API Success Response:', response.data); 
-
-    return response.status >= 200 && response.status < 300;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (error: any) {
-    if (axios.isAxiosError(error)) {
-      console.error('Error submitting form:', error.message);
-      if (error.response) {
-        console.error('Error response data:', error.response.data);
-        console.error('Error response status:', error.response.status);
-      } else if (error.request) {
-        console.error('No response received:', error.request);
-      } else {
-        console.error('Error setting up request:', error.message);
-      }
-    } else {
-      console.error('An unexpected error occurred:', error);
-    }
-    return false;
-  }
-};
