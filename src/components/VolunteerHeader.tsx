af258f29ae7546cd0a5d06eaa198f56d16884868
Olamide1705 <abdullateefnafisat@gmail.com>
2025-08-15 21:01:58 +0100

    Relevant Corrections Made

--- src/components/VolunteerHeader.tsx
+++ src/components/VolunteerHeader.tsx
@@ -6,19 +6,18 @@ import Volunteer from '../components/Volunteer';
 const VolunteerHeader = () => {
   return (
     <div className='scroll-smooth'>
-   <div className="flex flex-col justify-center items-center min-h-screen pb-20">
+   <div className="flex flex-col min-h-screen pb-20">
       <div className="relative min-h-screen pt-20 pb-20 flex items-center overflow-hidden">
       <div className="absolute inset-0">
-        <div className="absolute z-10"></div>
-        <img 
+         <img 
           src={rings} 
           alt="" 
-          className="w-full h-full object-cover bg-opacity-20"
+          className="w-full h-full object-cover"
         />
       </div>
       
-      <div className="container mx-auto px-4 md:px-6 relative z-20">
-        <div className="grid md:grid-cols-2 gap-12 items-center">
+      <div className="w-full px-4 md:px-10 relative z-20">
+        <div className="gap-12 items-center">
           <div className="text-center md:text-left">
             <div className="inline-block mb-6 animate-float">
               <span className="px-4 py-2 rounded-full bg-white/80 backdrop-blur-sm text-emerald-900 font-medium shadow-xl shadow-emerald-900/10 flex items-center gap-2">
@@ -36,7 +35,11 @@ const VolunteerHeader = () => {
               </span>
             </h1>
             
-            <p className="text-lg md:text-xl text-emerald-900/80 mb-8 leading-relaxed">
+            <p className="hidden md:block text-lg md:text-xl text-emerald-900/80 mb-8 leading-loose">
+              Join our community where experienced Muslim couples share wisdom to<br/> help build marriages that reflect the beauty of Islamic teachings.
+            </p>
+
+            <p className="md:hidden text-center text-lg md:text-xl text-emerald-900/80 mb-8 leading-relaxed">
               Join our community where experienced Muslim couples share wisdom to help build marriages that reflect the beauty of Islamic teachings.
             </p>
 
