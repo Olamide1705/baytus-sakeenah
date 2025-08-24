123f679b7dfc50cc6256804258b49e4bf7e90c60
Olamide1705 <abdullateefnafisat@gmail.com>
2025-08-24 17:25:07 +0100

    Volunteer Form fixed

--- src/components/VolunteerForm.tsx
+++ src/components/VolunteerForm.tsx
@@ -347,7 +347,7 @@ const VolunteerForm: React.FC = () => {
                 onClick={() => setSubmissionSuccess(false)}
                 className="mt-6 bg-teal-600 text-white font-bold py-2 px-5 rounded-lg hover:bg-teal-700 transition-colors"
               >
-                Ma'as-salam
+                Ma'as-Salam
               </button>
             </div>
           ) : (
